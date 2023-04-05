dfd008803c3ff0a3fd46609bd9f4140d391d394a
Faster Speeding <luke@lmbyrne.dev>
2023-04-05 03:06:55 +0100

    Update type and value comments

--- docs_src/links.py
+++ docs_src/links.py
@@ -33,15 +33,15 @@ def find_iter(app: hikari.RESTAware):
 async def invite_link(app: hikari.RESTAware):
     link = yuyo.links.InviteLink.from_link(app, "https://discord.gg/nekosmeowers")
 
-    link.code  # == "nekosmeowers"
-    await link.fetch()  # hikari.Invite
-    link.get()  # hikari.InviteWithMetadata | None
-    str(link)  # "https://discord.gg/nekosmeowers"
+    link.code  # value: "nekosmeowers"
+    await link.fetch()  # type: hikari.Invite
+    link.get()  # type: hikari.InviteWithMetadata | None
+    str(link)  # value: "https://discord.gg/nekosmeowers"
 
 
 def make_invite_link():
     link = yuyo.links.make_invite_link("codecode")
-    link  # "https://discord.gg/codecode"
+    link  # value: "https://discord.gg/codecode"
 
 
 async def message_link(app: hikari.RESTAware):
@@ -49,45 +49,45 @@ async def message_link(app: hikari.RESTAware):
         app, "https://discord.com/channels/54123123321123/2134432234342/56445234124"
     )
 
-    link.is_dm_link  # == False
-    link.guild_id  # == 54123123321123
-    link.channel_id  # == 2134432234342
-    link.message_id  # == 56445234124
-    await link.fetch()  # hikari.Message
-    link.get()  # hikari.Message | None
-    str(link)  # "https://discord.com/channels/54123123321123/2134432234342/56445234124"
+    link.is_dm_link  # value: False
+    link.guild_id  # value: 54123123321123
+    link.channel_id  # value: 2134432234342
+    link.message_id  # value: 56445234124
+    await link.fetch()  # type: hikari.Message
+    link.get()  # type: hikari.Message | None
+    str(link)  # value: "https://discord.com/channels/54123123321123/2134432234342/56445234124"
 
 
 def make_message_link():
     #                                  (channel_id, message_id)
     link = yuyo.links.make_message_link(654323412, 4534512332, guild=123321)
-    # link = # "https://discord.com/channels/123321/654323412/4534512332"
+    link  # value: "https://discord.com/channels/123321/654323412/4534512332"
     link = yuyo.links.make_message_link(333333333, 5555555555)
-    # link = # "https://discord.com/channels/@me/333333333/5555555555"
+    link  # value: "https://discord.com/channels/@me/333333333/5555555555"
 
 
 async def template_link(app: hikari.RESTAware):
     link = yuyo.links.TemplateLink.from_link(app, "https://discord.new/aaaaaaaaaa")
 
-    link.code  # == "aaaaaaaaaa"
-    await link.fetch()  # hikari.Template
-    str(link)  # == "https://discord.new/aaaaaaaaaa"
+    link.code  # value: "aaaaaaaaaa"
+    await link.fetch()  # type: hikari.Template
+    str(link)  # value: "https://discord.new/aaaaaaaaaa"
 
 
 def make_template_link():
     raw_link = yuyo.links.make_template_link("cododododoe")
-    raw_link  # == "https://discord.new/aaaaaaaaaa"
+    raw_link  # value: "https://discord.new/aaaaaaaaaa"
 
 
 async def webhook_link(app: hikari.RESTAware):
     link = yuyo.links.WebhookLink.from_link(app, "https://discord.com/api/webhooks/123321123/efsdfasdsa")
 
-    link.webhook_id  # == 123321123
-    link.token  # == "efsdfasdsa"
-    await link.fetch()  # hikari.IncomingWebhook
-    str(link)  # == "https://discord.com/api/webhooks/123321123/efsdfasdsa"
+    link.webhook_id  # value: 123321123
+    link.token  # value: "efsdfasdsa"
+    await link.fetch()  # type: hikari.IncomingWebhook
+    str(link)  # value: "https://discord.com/api/webhooks/123321123/efsdfasdsa"
 
 
 def make_webhook_link():
     raw_link = yuyo.links.make_webhook_link(123321, "hfdssdasd")
-    raw_link  # == "https://discord.com/api/webhooks/123321123/efsdfasdsa"
+    raw_link  # value: "https://discord.com/api/webhooks/123321123/efsdfasdsa"
