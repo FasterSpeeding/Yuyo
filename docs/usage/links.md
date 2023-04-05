24c2979dd3e72ecf7e3d16125c200e5d29e182e6
Faster Speeding <luke@lmbyrne.dev>
2023-04-05 02:06:00 +0100

    Add links section to usage guide

--- docs/usage/links.md
+++ docs/usage/links.md
@@ -1,1 +1,106 @@
 # Links
+
+Yuyo provides some utilities for handling Discord links in [yuyo.links][].
+
+### BaseLink
+
+There are several [BaseLink][yuyo.links.BaseLink] provided which come with 3
+methods for parsing them from strings.
+
+```py
+--8<-- "./docs_src/links.py:20:20"
+```
+
+[BaseLink.from_link][yuyo.links.BaseLink.from_link] lets you parse a raw link
+string. This is strict about validation and will raise a [ValueError][] if the
+full passed string isn't a match for the expected link structure.
+
+```py
+--8<-- "./docs_src/links.py:24:25"
+```
+
+[BaseLink.find][yuyo.links.BaseLink.find] lets you extract the first link in a
+string. This will search for a link at any point in the string and returns the
+parsed link object or [None][] if no link was found.
+
+```py
+--8<-- "./docs_src/links.py:29:30"
+```
+
+[BaseLink.find_iter][yuyo.links.BaseLink.find_iter] lets you iterate over the
+matching links in a string by iterating over link objects which were parsed
+from the string.
+
+### BaseLink implementations
+
+There are 4 implementations of [BaseLink][yuyo.links.BaseLink] provided by
+Yuyo (which all support the parsing methods listed above):
+
+##### Invite links
+
+```py
+--8<-- "./docs_src/links.py:34:39"
+```
+
+[InviteLink][yuyo.links.InviteLink] handles parsing invite links.
+
+```py
+--8<-- "./docs_src/links.py:43:44"
+```
+
+[links.make_invite_link][yuyo.links.make_invite_link] offers an alternative
+way to make an invite link string which doesn't require having a Bot or REST
+app in scope.
+
+##### Message links
+
+```py
+--8<-- "./docs_src/links.py:48:58"
+```
+
+[MessageLink][yuyo.links.MessageLink] handles parsing message links.
+
+[MessageLink.guild_id][yuyo.links.MessageLink.guild_id] will be [None][] for DM messages.
+
+```py
+--8<-- "./docs_src/links.py:62:66"
+```
+
+[links.make_message_link][yuyo.links.make_message_link] offers an alternative
+way to make a message link string which doesn't require having a Bot or REST
+app in scope.
+
+##### Guild template links
+
+```py
+--8<-- "./docs_src/links.py:70:74"
+```
+
+[TemplateLink][yuyo.links.TemplateLink] handles parsing guild template links.
+
+```py
+--8<-- "./docs_src/links.py:78:79"
+```
+
+[links.make_template_link][yuyo.links.make_template_link] offers an alternative
+way to make a template link string which doesn't require having a Bot or REST
+app in scope.
+
+##### Webhook links
+
+```py
+--8<-- "./docs_src/links.py:83:88"
+```
+
+[WebhookLink][yuyo.links.WebhookLink] handles parsing webhook links.
+
+This class inherits from [hikari.ExecutableWebhook][hikari.webhooks.ExecutableWebhook] and
+therefore has all the webhook execute methods you'll find on interaction and webhook objects.
+
+```py
+--8<-- "./docs_src/links.py:92:93"
+```
+
+[links.make_webhook_link][yuyo.links.make_webhook_link] offers an alternative
+way to make a webhook link string which doesn't require having a Bot or REST
+app in scope.
--- docs_src/links.py
+++ docs_src/links.py
@@ -8,3 +8,86 @@
 #
 # You should have received a copy of the CC0 Public Domain Dedication along with this software.
 # If not, see <https://creativecommons.org/publicdomain/zero/1.0/>.
+
+# pyright: reportUnusedVariable=none
+# pyright: reportUnusedExpression=none
+import hikari
+
+import yuyo
+
+
+def from_link(app: hikari.RESTAware):
+    link = yuyo.links.TemplateLink.from_link(app, "https://discord.new/aaaaaaaaaa")
+
+
+async def find(app: hikari.RESTAware):
+    if link := yuyo.links.InviteLink.find(app, "meow you can nyaa us at discord.gg/nekosmeowers"):
+        ...
+
+
+def find_iter(app: hikari.RESTAware):
+    for link in yuyo.links.MessageLink.find_iter(app, "message content"):
+        ...
+
+
+async def invite_link(app: hikari.RESTAware):
+    link = yuyo.links.InviteLink.from_link(app, "https://discord.gg/nekosmeowers")
+
+    link.code  # == "nekosmeowers"
+    await link.fetch()  # hikari.Invite
+    link.get()  # hikari.InviteWithMetadata | None
+    str(link)  # "https://discord.gg/nekosmeowers"
+
+
+def make_invite_link():
+    link = yuyo.links.make_invite_link("codecode")
+    link  # "https://discord.gg/codecode"
+
+
+async def message_link(app: hikari.RESTAware):
+    link = yuyo.links.MessageLink.from_link(
+        app, "https://discord.com/channels/54123123321123/2134432234342/56445234124"
+    )
+
+    link.is_dm_link  # == False
+    link.guild_id  # == 54123123321123
+    link.channel_id  # == 2134432234342
+    link.message_id  # == 56445234124
+    await link.fetch()  # hikari.Message
+    link.get()  # hikari.Message | None
+    str(link)  # "https://discord.com/channels/54123123321123/2134432234342/56445234124"
+
+
+def make_message_link():
+    #                                  (channel_id, message_id)
+    link = yuyo.links.make_message_link(654323412, 4534512332, guild=123321)
+    # link = # "https://discord.com/channels/123321/654323412/4534512332"
+    link = yuyo.links.make_message_link(333333333, 5555555555)
+    # link = # "https://discord.com/channels/@me/333333333/5555555555"
+
+
+async def template_link(app: hikari.RESTAware):
+    link = yuyo.links.TemplateLink.from_link(app, "https://discord.new/aaaaaaaaaa")
+
+    link.code  # == "aaaaaaaaaa"
+    await link.fetch()  # hikari.Template
+    str(link)  # == "https://discord.new/aaaaaaaaaa"
+
+
+def make_template_link():
+    raw_link = yuyo.links.make_template_link("cododododoe")
+    raw_link  # == "https://discord.new/aaaaaaaaaa"
+
+
+async def webhook_link(app: hikari.RESTAware):
+    link = yuyo.links.WebhookLink.from_link(app, "https://discord.com/api/webhooks/123321123/efsdfasdsa")
+
+    link.webhook_id  # == 123321123
+    link.token  # == "efsdfasdsa"
+    await link.fetch()  # hikari.IncomingWebhook
+    str(link)  # == "https://discord.com/api/webhooks/123321123/efsdfasdsa"
+
+
+def make_webhook_link():
+    raw_link = yuyo.links.make_webhook_link(123321, "hfdssdasd")
+    raw_link  # == "https://discord.com/api/webhooks/123321123/efsdfasdsa"
