d94a68bf46b6a95bdd4c549ce6937d59bf3bf9fa
FasterSpeeding <lucina@lmbyrne.dev>
2024-04-21 16:32:17 +0100

    Update components.py

--- yuyo/components.py
+++ yuyo/components.py
@@ -2726,7 +2726,7 @@ class StreamExecutor(AbstractComponentExecutor, timeouts.AbstractTimeout):
         self,
         *,
         authors: typing.Optional[collections.Iterable[hikari.SnowflakeishOr[hikari.User]]],
-        custom_ids: collection.Collection[str] = (),
+        custom_ids: collection.Collections[str] = (),
         ephemeral_default: bool = False,
         max_backlog: int = 5,
         timeout: typing.Union[float, int, datetime.timedelta, None],
